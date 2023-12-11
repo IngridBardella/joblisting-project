893838d76ebc2029b266b70c9a66d19c97333923
dog-at-the-window <134427736+dog-at-the-window@users.noreply.github.com>
2023-12-10 20:11:28 -0500

    added CRUD operations

--- project/src/main/java/com/example/project/service/ListingService.java
+++ project/src/main/java/com/example/project/service/ListingService.java
@@ -1,14 +1,13 @@
 package com.example.project.service;
 
 import com.example.project.dto.ListingDTO;
-import com.example.project.entity.Listing;
 
 import java.util.List;
 
 public interface ListingService {
     List<ListingDTO> getAllListings();
 
-    Long saveListing(ListingDTO listingDTO);
+    String saveListing(ListingDTO listingDTO);
 
     ListingDTO getListingById(Long id);
 
--- project/src/main/java/com/example/project/service/ListingServiceImpl.java
+++ project/src/main/java/com/example/project/service/ListingServiceImpl.java
@@ -1,11 +1,13 @@
 package com.example.project.service;
 
 import com.example.project.dto.ListingDTO;
+import com.example.project.entity.Listing;
 import com.example.project.mapper.ListingMapperHelper;
 import com.example.project.repository.ListingRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ListingServiceImpl implements ListingService {
@@ -20,21 +22,29 @@ public class ListingServiceImpl implements ListingService {
 
     @Override
     public List<ListingDTO> getAllListings() {
-        return null;
+        List<Listing> listings = listingRepository.findAll();
+        return mapperHelper.convertListingListToListingDTOList(listings);
     }
 
     @Override
-    public Long saveListing(ListingDTO listingDTO) {
-        return null;
+    public String saveListing(ListingDTO listingDTO) {
+        Listing listing = mapperHelper.convertListingDTOToListing(listingDTO);
+        Listing savedListing = listingRepository.save(listing);
+        return savedListing.getJobTitle();
     }
 
     @Override
     public ListingDTO getListingById(Long id) {
-        return null;
+        Optional<Listing> byId = listingRepository.findById(id);
+        if(byId.isPresent()) {
+            Listing found = byId.get();
+            return mapperHelper.convertListingToListingDTO(found);
+        }
+        throw new RuntimeException("Unable to find listing.");
     }
 
     @Override
     public void deleteListingById(Long id) {
-
+        listingRepository.deleteById(id);
     }
 }
